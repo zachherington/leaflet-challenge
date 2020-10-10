a488be4231e737fc88f56948117718540e8d8bbb
Zachary Herington <zachherington@gmail.com>
2020-10-10 19:14:16 -0400

    api key reference error fixed.

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,1 +1,2 @@
-const API_KEY = "pk.eyJ1IjoiemhlcmluZ3QiLCJhIjoiY2tmcmdiYmZvMHRidjJ6cHNwdmxhZjd2OSJ9.oL6e1kSx_P2O5a0ZVhQMvA"
+// API key
+const API_KEY = "pk.eyJ1IjoiemhlcmluZ3QiLCJhIjoiY2tmcmdiYmZvMHRidjJ6cHNwdmxhZjd2OSJ9.oL6e1kSx_P2O5a0ZVhQMvA";
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,11 +1,13 @@
 geojson_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
+
+
 var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
 	attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
 	maxZoom: 18,
 	id: "mapbox.satellite",
-	accessToken: "pk.eyJ1IjoiemhlcmluZ3QiLCJhIjoiY2tmcmdiYmZvMHRidjJ6cHNwdmxhZjd2OSJ9.oL6e1kSx_P2O5a0ZVhQMvA"
-});
+	accessToken: API_KEY
+});	
 
 
 var myMap = L.map("map", {
